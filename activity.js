c40e694ef50fbde2c8c8b11a335c1dd0d1a76e8c
Christy Cooke <cleecooke@gmail.com>
2021-05-09 23:35:29 -0600

    final try

---
+++ activity.js
@@ -0,0 +1,25 @@
+// ***** Make the timer *****
+
+// function convertSeconds(s) {
+//     let min = floor(s / 60);
+//     var sec = s % 60;
+//     return min + ":" + sec;
+// }
+
+// var counter = 0;
+// var timeLeft = 60;
+// function setup() {
+//     noCanvas();
+
+//     let timer = select("#timer)";
+//     timer.html(convertSeconds(timeLeft - counter));
+
+//     funtion timeIt() {
+//         counter++;
+//         timer.html(convertSeconds)timeLeft - counter));
+
+//     }
+
+//     setInterval(timeIt, 1000);
+// }
+// setInterval(timeIt, 1000);
